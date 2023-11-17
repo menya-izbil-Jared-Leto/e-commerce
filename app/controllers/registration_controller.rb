1023e40863c23b75752b7fdd1f92d65e5293f6f3
menya-izbil-Jared-Leto <mxmvmxm1k@gmail.com>
2023-11-17 22:02:09 +0300

    Add profile editing and deleting

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,12 +2,23 @@ class RegistrationController < ApplicationController
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+    #изменение данных
+    def configure_account_update_params
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name])
+    end
 
+    protected
+    #ввод данных
     def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     end
     
+    def update_resource (resource,params)
+        return super if params["password"]&.present?
+    
+        resource.update_without_password(params.except "current_password")
+    end
+
     def index
     end
 end
